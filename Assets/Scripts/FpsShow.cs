8cb1010bf6656f657f6458f77c60f042dad169bf
weipengsong <sdswp@126.com>
2019-02-16 17:14:06 +0800

    显示帧数

---
+++ Assets/Scripts/FpsShow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsShow : MonoBehaviour
+{
+    [SerializeField]
+    private Text FpsTxt;
+
+    private float lastTime = 0;
+    private float currentTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = -1;
+        lastTime = 0;
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = Time.time;
+        FpsTxt.text = ((int)(1/(currentTime - lastTime))).ToString();
+        lastTime = currentTime;
+    }
+}
